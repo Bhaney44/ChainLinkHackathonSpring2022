94c03d8701030652b48b8927781cdf77a26e6ce2
B Haney <43055154+Bhaney44@users.noreply.github.com>
2022-05-14 12:04:43 -0700

    Add files via upload

---
+++ LinkContracts/pseudocode.sol
@@ -0,0 +1,52 @@
+// Copyright Brian Haney 2022
+// Apache License
+// Backend architecture prototyping
+
+// Purpose
+// The goal here is to build a smart contract that sends assets based on two parameters.
+// The first parameter is a user entered address.
+// The second parameter is a user entered amount.
+// This pseudocode is intended for logical development only.
+// This pseudocode is not intended for use or deployment.
+
+// Software version
+pragma solidity 0.8.7;
+
+// Logic for contract zero
+/////////////////////////////////
+contract Converter_Zero{
+    address payable reciever;
+    unit public amount = 0
+    modifier reciever(address){
+        address payable reciever
+    }
+    modifier amount(unit){
+        unit public amount
+    }
+    function convert() {
+        address payable reciever;
+        unit public amount = amount
+    }
+
+}
+/////////////////////////////////
+
+// Logic for contract one
+/////////////////////////////////
+contract Converter_One{
+    // Reciever and amount variables
+    reciever = user_address_entry
+    amount = user_amount_entry
+    // Sends assets
+    function send(address receiver, uint amount) public {
+        if (amount > balances[msg.sender])
+            revert InsufficientBalance({
+                requested: amount,
+                available: balances[msg.sender]
+            });
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
+    }
+}
+/////////////////////////////////
