766555f01351bc14dd96ed6668d0285130e0924e
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-05-24 09:26:27 +0100

    adding home backend URL ➫⏚

--- converter-backend/app.ts
+++ converter-backend/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import config from "config";
 import connect from "./db";
+import { Response, Request } from "express";
 
 
 const port = config.get("port") as number;
@@ -12,6 +13,11 @@ const app = express();
 // Parses incoming requests with JSON payloads
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//home URl
+app.get('/', (req : Request,res : Response) => {
+    res.json('goLink DB 🔞 is running')
+})
 
 app.listen(port, host, () => {
   console.log(`Server listening at http://${host}:${port}`)
