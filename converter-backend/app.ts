ef0992728545b791d6000694b480269401849ac1
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-05-24 14:23:33 +0100

    updating middlewares 🔞

--- converter-backend/app.ts
+++ converter-backend/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import config from "config";
+import dotenv from "dotenv";
 import connect from "./db";
+import cors from "cors";
 import { Response, Request } from "express";
 import { handleCreateUser } from "./src/controller";
 
@@ -8,12 +10,18 @@ import { handleCreateUser } from "./src/controller";
 const port = config.get("port") as number;
 const host = config.get("host") as string;
 
+//using .env config
+dotenv.config();
+
 const app = express();
 
 
 // Parses incoming requests with JSON payloads
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//middlewares
+app.use(cors());
 
 //home URl
 app.get('/', (req : Request,res : Response) => {
