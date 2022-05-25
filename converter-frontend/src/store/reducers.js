12409a70effde9a632599408f84b627c1cc1a5ee
Aspirin 💆🏽 <samuellyworld@gmail.com>
2022-05-26 00:23:15 +0100

    updating --frontend 📲

--- converter-frontend/src/store/reducers.js
+++ converter-frontend/src/store/reducers.js
@@ -2,10 +2,7 @@ import { combineReducers } from "redux";
 
 const status = (
   state = {
-    darkTheme: localStorage.getItem("mode") === "light" ? false : true,
     alertModal: { openModal: false, modalContent: "" },
-    electModal: { openElectModal: false, modalData: null },
-    voteModal: { openModalVote: false, voteData: null },
     confirmWallet: { openWallet: false, walletContent: ""},
     addressNum: 0,
     address: null,
@@ -28,11 +25,7 @@ const status = (
         alertModal: { openModal: true, modalContent: action.alertContent },
       };
 
-      case "light_mode":
-        return { ...state, darkTheme: false };
-      case "dark_mode":
-        return { ...state, darkTheme: true };
-
+  
     case "close_modal":
       return { ...state, alertModal: { openModal: false, modalContent: "" } };
 
